f417060b34f14422d8a36d93039ebc165cb6a58f
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-15 19:53:02 +0100

    html bones are there

--- src/pages/Signup/Form/Form.js
+++ src/pages/Signup/Form/Form.js
@@ -1,31 +1,40 @@
 import React from "react";
 import Styles from "./Form.module.css";
+import Details from "./Questions/Details/Details"
 
 function Form() {
   return (
     <div>
-      <div className={Styles.headerWrapper}>
-        <h2 className={Styles.header}>SignUp</h2>
-        <nav className={Styles.navBar}>
-          <ul>
-            <li>Details</li>
-            <li>Preferences</li>
-            <li>Confirm</li>
-          </ul>
-        </nav>
+      <div className={Styles.addBreathingSpace}>
+        <div className={Styles.headerWrapper}>
+          <h2 className={Styles.header}>SignUp</h2>
+          <nav className={Styles.navBar}>
+            <ul>
+              <li>Details</li>
+              <li>Preferences</li>
+              <li>Confirm</li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <p>
+            Man-of-war lateen sail spirits gunwalls gibbet tackle reef sails
+            scuppers hands splice the main brace. Man-of-war plunder Davy Jones'
+            Locker Buccaneer smartly keelhaul dance the hempen jig yo-ho-ho
+            Plate Fleet list. Handsomely Pieces of Eight cable no prey, no pay
+            lanyard Letter of Marque quarterdeck gangplank rum lookout. Aye log
+            cutlass matey execution dock pressgang Shiver me timbers grog
+            blossom jury mast pinnace. Cutlass hail-shot tender crimp Corsair me
+            fore ye splice the main brace chandler. Nipperkin execution dock
+            bowsprit smartly bring a spring upon her cable gally tack fore brig
+            spirits.
+          </p>
+        </div>
       </div>
       <div>
-        <p>
-          Man-of-war lateen sail spirits gunwalls gibbet tackle reef sails
-          scuppers hands splice the main brace. Man-of-war plunder Davy Jones'
-          Locker Buccaneer smartly keelhaul dance the hempen jig yo-ho-ho Plate
-          Fleet list. Handsomely Pieces of Eight cable no prey, no pay lanyard
-          Letter of Marque quarterdeck gangplank rum lookout. Aye log cutlass
-          matey execution dock pressgang Shiver me timbers grog blossom jury
-          mast pinnace. Cutlass hail-shot tender crimp Corsair me fore ye splice
-          the main brace chandler. Nipperkin execution dock bowsprit smartly
-          bring a spring upon her cable gally tack fore brig spirits.
-        </p>
+          <div className={Styles.leftArrow}></div>
+            <Details/>
+          <div className={Styles.RightArrow}></div>
       </div>
     </div>
   );
---
+++ src/pages/Signup/Form/Questions/Details/Details.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function Details() {
+
+  return (<h2>Heyy</h2>)
+}
+
+export default Details;
