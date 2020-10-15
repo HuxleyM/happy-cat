5406b8e353d174fa8a1b6b899e93c9c325bfde36
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-15 16:20:00 +0100

    have basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Styles from'./App.module.css';
 import Signup from './Signup/Signup'
 
 function App() {
   return (
     <div className="App">
-        <header>
+        <header className={Styles.mainHeader}>
             <h1>HappyCat.</h1>
         </header>
         <Signup/>
