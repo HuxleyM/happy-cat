477cb9a7e22983de23045ab3852043e86f296baf
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-15 16:05:40 +0100

    Folder structure set up

---
+++ src/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Signup.css';
+
+function Signup(){
+
+    // logic for which component to render
+    return (
+        <div id='signUp-wrapper'>
+            
+        </div>
+    )
+}
