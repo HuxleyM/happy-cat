c24b6d7fa2e999570e67253ad78821a63ca48fc9
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-15 19:40:42 +0100

    Style adding style to form

--- src/pages/Signup/Form/Form.js
+++ src/pages/Signup/Form/Form.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React from "react";
+import Styles from "./Form.module.css";
 
 function Form() {
-    return (
-        <div>
-            <div>
-                <h2>SignUp</h2>
-                <nav>
-                    <ul>
-                        <li>Details</li>
-                        <li>Preferences</li>
-                        <li>Confirm</li>
-                    </ul>
-                </nav>
-            </div>
-            <div>
-                <p>
-                    Man-of-war lateen sail spirits gunwalls gibbet tackle reef sails scuppers hands splice the main brace. Man-of-war plunder Davy Jones' Locker Buccaneer smartly keelhaul dance the hempen jig yo-ho-ho Plate Fleet list. Handsomely Pieces of Eight cable no prey, no pay lanyard Letter of Marque quarterdeck gangplank rum lookout.
-
-                    Aye log cutlass matey execution dock pressgang Shiver me timbers grog blossom jury mast pinnace. Cutlass hail-shot tender crimp Corsair me fore ye splice the main brace chandler. Nipperkin execution dock bowsprit smartly bring a spring upon her cable gally tack fore brig spirits.
+  return (
+    <div>
+      <div className={Styles.headerWrapper}>
+        <h2 className={Styles.header}>SignUp</h2>
+        <nav className={Styles.navBar}>
+          <ul>
+            <li>Details</li>
+            <li>Preferences</li>
+            <li>Confirm</li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <p>
+          Man-of-war lateen sail spirits gunwalls gibbet tackle reef sails
+          scuppers hands splice the main brace. Man-of-war plunder Davy Jones'
+          Locker Buccaneer smartly keelhaul dance the hempen jig yo-ho-ho Plate
+          Fleet list. Handsomely Pieces of Eight cable no prey, no pay lanyard
+          Letter of Marque quarterdeck gangplank rum lookout. Aye log cutlass
+          matey execution dock pressgang Shiver me timbers grog blossom jury
+          mast pinnace. Cutlass hail-shot tender crimp Corsair me fore ye splice
+          the main brace chandler. Nipperkin execution dock bowsprit smartly
+          bring a spring upon her cable gally tack fore brig spirits.
         </p>
-            </div>
-
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Form;
