0de25950683f6060892e09cce7db9aeb4904e701
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-15 17:02:35 +0100

    implemented context and provider hopefully have state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Styles from'./App.module.css';
+import { UserContext } from './Context/userContext';
 import Signup from './Signup/Signup'
 
 function App() {
+    const [user, setUser] = useState({
+        completed: false,
+    })
+    const providerValue = useMemo({user, setUser}),[user,setUser]
   return (
     <div className="App">
-        <header className={Styles.mainHeader}>
-            <h1>HappyCat.</h1>
-        </header>
-        <body className={Styles.mainBody}>
-            <Signup/>
-        </body>
+        <UserContext.Provider value={providerValue}>
+            <header className={Styles.mainHeader}>
+                <h1>HappyCat.</h1>
+            </header>
+            <body className={Styles.mainBody}>
+                <Signup/>
+            </body>
+        </UserContext.Provider>
     </div>
   );
 }
