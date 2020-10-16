75ee726d53cb1643b87ff98a59bc59d0ed5b0894
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-16 18:43:07 +0100

    feat coded as running out of time

--- src/pages/Signup/Form/Questions/Details/Details.js
+++ src/pages/Signup/Form/Questions/Details/Details.js
@@ -1,57 +1,103 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Styles from "./Details.module.css";
-import * as utils from './Details'
+import * as utils from "./Details";
+import { UserContext } from '../../../../../Context/userContext';
 
-export const handleFormSubmission = () => {
-    if(validPassword()){
-        
-    }
+export const validEmail = () => {
+  const email = document.getElementById("email").value;
+  const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+  return !!email.match(emailRegex);
 };
 
-export const validPassword = () => {
+export const collectUserDetails = () => {
+    const userName = document.getElementById('userName').value;
     const password = document.getElementById('password').value;
-    const passwordRegex = /^(?=.*\d)(?=.*\W).{8,}$/g
-    // if (!password.match(passwordRegex)) {
-    //     return {...errors, password: 'This is not a valid password, include digits, special chars and be at least 8 digits long'}
-    // } 
-    // double here to return if theres a match or not
-    return !!password.match(passwordRegex)
+    const email = document.getElementById('email').value;
+    return {userName, password, email}
 }
 
+export const validPassword = () => {
+  const password = document.getElementById("password").value;
+  const passwordRegex = /^(?=.*\d)(?=.*\W).{8,}$/g;
+  return !!password.match(passwordRegex);
+};
 
-export const checkFieldsMatch = (errors, fields) => {
-      const first = document.getElementById(`${fields.first}`).value;
-      const second = document.getElementById(`${fields.second}`).value;
-      if (first !== second) {
-        const newErrors = {...errors}
-        newErrors[`${fields.second}`] = { error: `${fields.first}'s do not match`}
-        return newErrors
-      } else {
-          const current = { ...errors };
-          if(current[`${fields.second}`]){
-            delete current[`${fields.second}`];
-          }
-          return current;
-      }
-  };
-
-
-
-
-
+export const checkFieldsMatch = ({first, second}) => {
+  const firstValue = document.getElementById(`${first}`).value;
+  const secondValue = document.getElementById(`${second}`).value;
+  return (firstValue === secondValue)
+};
 
 function Details(props) {
+  const {user, setUser} = useContext(UserContext)
   const [errors, setErrors] = useState({});
-  const [formDetails, setFormDetails] = useState({})
+
+  const setErrorsSwitch = ({field, mode}) =>{
+    switch(field){
+        case 'password':   
+            if(mode === 'ADD'){
+                setErrors({...errors, password: 'This is not a valid password, include digits, special chars and be at least 8 digits long'})
+            }
+            let passwordErrors = {...errors}
+            delete passwordErrors.password
+            setErrors({...passwordErrors})
+            break;
+        case 'email':
+            if(mode === 'ADD'){
+                setErrors({...errors, email: 'This is not a valid email'})
+            }
+            let emailErrors = {...errors}
+            delete emailErrors.email
+            setErrors({...emailErrors})
+            break;
+        case 'emailRetype':
+            if(mode === 'ADD'){
+                setErrors({...errors, emailRetype: 'Emails do not match'})
+            }
+            let emailRErrors = {...errors}
+            delete emailRErrors.emailRErrors
+            setErrors({...emailRErrors})
+            break
+        case 'passwordRetype':
+            if(mode === 'ADD'){
+                setErrors({...errors, passwordRetype: 'Password do not match'})
+            }
+            let passwordReErrros = {...errors}
+            delete passwordReErrros.passwordReErrros
+            setErrors({...passwordReErrros})
+
+            break
+        default:
+            return
+    }
+}
+  
+  const runChecks = () => {
+    if (!utils.validPassword()) setErrorsSwitch({field:'password', mode:'ADD'})
+    if (!utils.validEmail()) setErrorsSwitch({field:'email', mode:'ADD'})
+    if (!utils.checkFieldsMatch({first:'email', second:'emailRetype'})) setErrorsSwitch({field:'emailRetype', mode:'ADD'})
+    if (!utils.checkFieldsMatch({first:'password', second:'passwordRetype'})) setErrorsSwitch({field:'passwordRetype', mode:'ADD'})
+}
+
+   const handleFormSubmission = (errors) => {
+    runChecks()
+    if(errors) return 
+    let userDetails = utils.collectUserDetails()
+    setUser({...user, userDetails})
+    let questionsAnswered = user.questionsAnswered += 1
+    let currentlyOnQuestion = user.currentlyOnQuestion += 1
+    setUser({...user, questionsAnswered, currentlyOnQuestion})
+};
 
 
 
+ 
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        utils.handleFormSubmission();
+        handleFormSubmission();
       }}
     >
       <div className={Styles.flexContainer}>
@@ -76,7 +122,6 @@ function Details(props) {
             <label htmlFor="emailRetype">Retype email:</label>
             <div>
               <input
-  
                 type="email"
                 id="emailRetype"
                 name="emailRetype"
@@ -126,7 +171,7 @@ function Details(props) {
         </div>
       </div>
 
-      <input type="submit" id='submitDetailsForm' value="Save and next"></input>
+      <input type="submit" id="submitDetailsForm" value="Save and next"></input>
     </form>
   );
 }
