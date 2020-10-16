7f744fa3d03d4d89ca2b6bf6e47f65ffa4b059be
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-17 00:03:42 +0100

    fixed a test finish tomorrow

--- src/pages/Signup/Form/Form.test.js
+++ src/pages/Signup/Form/Form.test.js
@@ -1,77 +1,95 @@
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-import Form from './Form';
-import Details from './Questions/Details/Details';
-import Preferences from './Questions/Preferences/Preferences';
-import Confirm from './Questions/Confirm/Confirm'
-import { UserContext } from '../../../Context/userContext';
-import * as utils from './Form';
+import React from "react";
+import { mount, shallow } from "enzyme";
+import Form from "./Form";
+import Details from "./Questions/Details/Details";
+import Preferences from "./Questions/Preferences/Preferences";
+import Confirm from "./Questions/Confirm/Confirm";
+import { UserContext } from "../../../Context/userContext";
+import * as utils from "./Form";
 
-describe('Form component', () => {
+describe("Form component", () => {
+  it("Should color the nav bar in respect to a users position in the questions", () => {});
 
-    it('Should color the nav bar in respect to a users position in the questions', () => {
+  it("If a user is on question 1 render Details", () => {
+    const context = { user: { currentlyOnQuestion: 0 } };
+    const wrapper = mount(
+      <UserContext.Provider value={context}>
+        <Form />
+      </UserContext.Provider>
+    );
+    expect(wrapper.containsMatchingElement(<Details />)).toEqual(true);
+    expect(wrapper.containsMatchingElement(<Preferences />)).toEqual(false);
+    expect(wrapper.containsMatchingElement(<Confirm />)).toEqual(false);
+  });
+  it("If a user is on question 2 render Preferences", () => {
+    const context = { user: { currentlyOnQuestion: 1 } };
+    const wrapper = mount(
+      <UserContext.Provider value={context}>
+        <Form />
+      </UserContext.Provider>
+    );
+    expect(wrapper.containsMatchingElement(<Details />)).toEqual(false);
+    expect(wrapper.containsMatchingElement(<Preferences />)).toEqual(true);
+    expect(wrapper.containsMatchingElement(<Confirm />)).toEqual(false);
+  });
+  it("If a user is on question 3 render Confirm", () => {
+    const context = {
+      user: {
+          // need to remember that this bit is gonna be wierd
+        currentlyOnQuestion: 2,
+        questionsAnswered: 3,
+        userDetails: {
+          userName: "kipo",
+          password: "theW0nder!",
+          email: "kipo@wondbe.com",
+        },
+        userPreference: { gifRate: 4, allowDogs: "sometimes" },
+      },
+    };
+    const wrapper = mount(
+      <UserContext.Provider value={context}>
+        <Form />
+      </UserContext.Provider>
+    );
+    expect(wrapper.containsMatchingElement(<Details />)).toEqual(false);
+    expect(wrapper.containsMatchingElement(<Preferences />)).toEqual(false);
+    expect(wrapper.containsMatchingElement(<Confirm />)).toEqual(true);
+  });
 
-    })
+  describe("Navigation", () => {
+    xit("if i have answered all questions i should be able to navigate back and forth", () => {
+      const context = {
+        user: {
+          currentlyOnQuestion: 3,
+          questionsAnswered: 3,
+          user: {
+            userDetails: {
+              userName: "kipo",
+              password: "theW0nder!",
+              email: "kipo@wondbe.com",
+            },
+            userPreference: { gifRate: 4, allowDogs: "sometimes" },
+          },
+        },
+      };
+      const wrapper = mount(
+        <UserContext.Provider value={context}>
+          <Form />
+        </UserContext.Provider>
+      );
 
-    it('If a user is on question 1 render Details', ()=>{
-        const context = {user:{ currentlyOnQuestion:0}};
-        const wrapper = mount(
-          <UserContext.Provider value={context}>
-            <Form />
-          </UserContext.Provider>,
-        );
-        expect(wrapper.containsMatchingElement(<Details />)).toEqual(true);
-        expect(wrapper.containsMatchingElement(<Preferences />)).toEqual(false);
-        expect(wrapper.containsMatchingElement(<Confirm />)).toEqual(false);
-    })
-    it('If a user is on question 2 render Preferences', ()=>{
-        const context = {user:{ currentlyOnQuestion:1}};
-        const wrapper = mount(
-          <UserContext.Provider value={context}>
-            <Form />
-          </UserContext.Provider>,
-        );
-        expect(wrapper.containsMatchingElement(<Details />)).toEqual(false);
-        expect(wrapper.containsMatchingElement(<Preferences />)).toEqual(true);
-        expect(wrapper.containsMatchingElement(<Confirm />)).toEqual(false);
-    })
-    xit('If a user is on question 3 render Confirm', ()=>{
-        const context = {user:{ currentlyOnQuestion:2}};
-        const wrapper = mount(
-          <UserContext.Provider value={context}>
-            <Form />
-          </UserContext.Provider>,
-        );
-        expect(wrapper.containsMatchingElement(<Details />)).toEqual(false);
-        expect(wrapper.containsMatchingElement(<Preferences />)).toEqual(false);
-        expect(wrapper.containsMatchingElement(<Confirm />)).toEqual(true);
-    })
+      const mock = jest.spyOn(utils, "movePage");
+      const handleClick = jest.spyOn(React, "useContext");
+      handleClick.mockImplementation((index) => [index, movePage]);
+      const backButton = wrapper.find(".leftArrow");
+      const forwardButton = wrapper.find(".rightArrow");
+      backButton.simulate("click");
+      expect(mock).toHaveBeenCalled();
+      expect(movePage).toBeTruthy();
+    });
 
-    describe('Navigation', ()=>{
-        xit('if i have answered all questions i should be able to navagate back and forth', ()=> {
-            const context = { user:{ currentlyOnQuestion:3, questionsAnswered:3}}
-            const wrapper = mount(         
-            <UserContext.Provider value={context}>
-                <Form />
-            </UserContext.Provider>,)
+    it("I cannot click forward if i havent answered/has errors the question", () => {});
 
-        const mock = jest.spyOn(utils, 'movePage')
-        const handleClick = jest.spyOn(React, "useContext");
-        handleClick.mockImplementation(index => [index, movePage]);
-        const backButton = wrapper.find('.leftArrow')
-        const forwardButton = wrapper.find('.rightArrow')
-        backButton.simulate('click')
-        expect(mock).toHaveBeenCalled();
-        expect(movePage).toBeTruthy();
-        })
-
-        it('I cannot click forward if i havent answered/has errors the question', () => {
-
-        })
-
-
-        it('I cannot click forward if i havent answered/has errors the question', () => {
-
-        })
-    })
-})
+    it("I cannot click forward if i havent answered/has errors the question", () => {});
+  });
+});
