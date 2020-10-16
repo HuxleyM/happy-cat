1da0507953c7c42ee3c28125b02676ada71aa3af
Huxley Millard <huxley@Huxleys-MacBook-Pro.local>
2020-10-16 11:17:40 +0100

    form is fully accesbile and nice

--- src/pages/Signup/Form/Questions/Details/Details.js
+++ src/pages/Signup/Form/Questions/Details/Details.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Styles from "./Details.module.css";
-import Button from '../../../../../Components/Button/Button'
+import Button from "../../../../../Components/Button/Button";
 
 function Details() {
+    const [errors, setErrors] = useState({}) 
+    const isError = ({field}) => false;
+
+
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
@@ -10,11 +15,15 @@ function Details() {
           <div className={Styles.questionWrapper}>
             <label for="userName">UserName:</label>
             <input type="text" id="userName" name="userName" required></input>
+            <div>{isError({field:"userName"}) && <p>error here</p>}</div>
           </div>
+
           <div className={Styles.questionWrapper}>
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required></input>
+            <div>{isError({field:"email"}) && <p>error here</p>}</div>
           </div>
+
           <div className={Styles.questionWrapper}>
             <label for="emailRetype">Retype email:</label>
             <input
@@ -23,19 +32,35 @@ function Details() {
               name="emailRetype"
               required
             ></input>
+            <div>{isError({field:"emailRetype"}) && <p>error here</p>}</div>
           </div>
         </div>
+
         <div className={Styles.rightSide}>
-        <div className={Styles.questionWrapper}>
+          <div className={Styles.questionWrapper}>
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required></input>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+            ></input>
+            <div>{isError({field:"password"}) && <p>error here</p>}</div>
           </div>
+
           <div className={Styles.questionWrapper}>
             <label for="passwordRetype">Retype password::</label>
-            <input type="password" id="passwordRetype" name="passwordRetype" required></input>
+            <input
+              type="password"
+              id="passwordRetype"
+              name="passwordRetype"
+              required
+            ></input>
+            <div>{isError({field:"passwordRetype"}) && <p>error here</p>}</div>
           </div>
         </div>
-        <Button text='save and next' value='incomplete'/>
+
+        <input type="submit" value="Save and next"></input>
       </form>
     </div>
   );
